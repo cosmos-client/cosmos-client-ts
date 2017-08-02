b90275e64687bdb35b9af3af185389b98adb1ad4
Matt Bell <mappum@gmail.com>
2017-08-02 18:15:07 -0400

    Added key methods

--- index.js
+++ index.js
@@ -3,6 +3,30 @@
 const request = require('request-promise-native')
 const old = require('old')
 
+function apiFunc (method, rootPath, propNames = [], nPathArgs = 0) {
+  return async function (...args) {
+    let path = rootPath
+
+    // add path args (e.g. 'name' in '/foo/${name}') to path
+    if (nPathArgs) {
+      let pathArgs = args.slice(0, nPathArgs)
+      path += '/' + pathArgs.join('/')
+    }
+
+    // create JSON object from list of property names and list of values
+    let data = {}
+    for (let i = 0; i < propNames.length; i++) {
+      data[propNames[i]] = args[i]
+    }
+
+    return await this.request(method, path, data)
+  }
+}
+
+const getReq = (...args) => apiFunc('GET', ...args)
+const postReq = (...args) => apiFunc('POST', ...args)
+const deleteReq = (...args) => apiFunc('DELETE', ...args)
+
 class Client {
   constructor (server = 'http://localhost:8998') {
     this.server = server
@@ -15,10 +39,14 @@ class Client {
       json: data || true
     })
   }
-
-  async generateKey (name, password) {
-    return await this.request('POST', '/keys', { name, password })
-  }
 }
+
+Object.assign(Client.prototype, {
+  generateKey: postReq('/keys', [ 'name', 'password' ]),
+  listKeys: getReq('/keys'),
+  getKey: getReq('/keys', [ 'name' ], 1),
+  updateKey: postReq('/keys', [ 'name', 'password', 'new_passphrase' ], 1),
+  deleteKey: deleteReq('/keys', [ 'name', 'password' ], 1)
+})
 
 module.exports = old(Client)
