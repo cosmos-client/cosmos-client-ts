208fd86eb902e9407931c4725f16c46868c7b69a
user <hiroaking717@gmail.com>
2019-07-27 00:46:56 +0900

    [update]型引数

--- src/x/supply/supply.ts
+++ src/x/supply/supply.ts
@@ -1,3 +1,18 @@
+import { CosmosSdkHost } from "../../common/cosmos-sdk-host";
+import { GetSupply } from "./types/total_supply";
+
 export module Supply {
+
+/**
+* @param host /supply/TotalSupply
+*/
+
+ export function getTotalSupply(host: CosmosSdkHost){
+    return host.get<GetSupply>()
+ }
+
+ export function getSupplyOf(host: CosmosSdkHost){
+     return host.get<GetSupply>()
+ }
 
 }
