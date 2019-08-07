ef488ece0a87a6b01b46d8f0b18d83b24835c804
user <hiroaking717@gmail.com>
2019-08-07 20:07:15 +0900

    [update]微修正

--- src/x/slashing/type/signing-info.ts
+++
@@ -1,8 +0,0 @@
-export interface signingInfo {
-    address: ConsAddress
-    start_height: Int32Array
-    index_offset: Int32Array
-    jailed_until: Time
-    tombstoned: boolean
-    missed_blocks_counter: Int32Array
-}
