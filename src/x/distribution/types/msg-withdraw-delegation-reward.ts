c2d9910a4ae48aed2b9776bd44e2d477f83433d1
user <hiroaking717@gmail.com>
2019-09-02 16:38:29 +0900

    [update]MsgWithdrawDelegationReward

--- src/x/distribution/types/msg-withdraw-delegation-reward.ts
+++ src/x/distribution/types/msg-withdraw-delegation-reward.ts
@@ -1,10 +1,26 @@
 import { Amino } from "../../../common/amino";
 import { Msg } from "../../../types/cosmos-sdk/msg";
+import { AccAddress } from "../../../types/cosmos-sdk/address/acc-address";
+import { ValAddress } from "../../../types/cosmos-sdk/address/val-address";
 
 @Amino.RegisterConcrete('cosmos-sdk/MsgWithdrawDelegationReward')
 export class MsgWithdrawDelegationReward implements Msg {
-/**
- * 
- * @param 
- */
+    /**
+     * 
+     * @param delegator_address
+     * @param validator_address
+     */
+    constructor(
+        public delegator_address: AccAddress,
+        public validator_address: ValAddress
+    ) { }
+    /**
+    * @see Amino.reviver
+    */
+   public static fromJSON(obj:any){
+       return new this(
+           AccAddress.fromBech32(obj.delegator_address),
+           ValAddress.fromBech32(obj.validator_address)
+       );
+   }
 }
