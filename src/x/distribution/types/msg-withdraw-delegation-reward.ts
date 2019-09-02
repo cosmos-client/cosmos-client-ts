f981e977c4817bcecbf389090c4b2045938f8d39
user <hiroaking717@gmail.com>
2019-09-02 16:44:32 +0900

    [update]MsgWithdrawDelegatorReward

--- src/x/distribution/types/msg-withdraw-delegation-reward.ts
+++ src/x/distribution/types/msg-withdraw-delegation-reward.ts
@@ -5,22 +5,5 @@ import { ValAddress } from "../../../types/cosmos-sdk/address/val-address";
 
 @Amino.RegisterConcrete('cosmos-sdk/MsgWithdrawDelegationReward')
 export class MsgWithdrawDelegationReward implements Msg {
-    /**
-     * 
-     * @param delegator_address
-     * @param validator_address
-     */
-    constructor(
-        public delegator_address: AccAddress,
-        public validator_address: ValAddress
-    ) { }
-    /**
-    * @see Amino.reviver
-    */
-   public static fromJSON(obj:any){
-       return new this(
-           AccAddress.fromBech32(obj.delegator_address),
-           ValAddress.fromBech32(obj.validator_address)
-       );
-   }
+   
 }
--- src/x/distribution/types/msg-withdraw-delegator-reward.ts
+++ src/x/distribution/types/msg-withdraw-delegator-reward.ts
@@ -1,5 +1,24 @@
 import { Msg } from "../../../types/cosmos-sdk/msg";
+import { AccAddress } from "../../../types/cosmos-sdk/address/acc-address";
+import { ValAddress } from "../../../types/cosmos-sdk/address/val-address";
 
 export class MsgWithdrawDelegatorReward implements Msg {
-  
+    /**
+         * 
+         * @param delegator_address
+         * @param validator_address
+         */
+    constructor(
+        public delegator_address: AccAddress,
+        public validator_address: ValAddress
+    ) { }
+    /**
+    * @see Amino.reviver
+    */
+    public static fromJSON(obj: any) {
+        return new this(
+            AccAddress.fromBech32(obj.delegator_address),
+            ValAddress.fromBech32(obj.validator_address)
+        );
+    }
 }
