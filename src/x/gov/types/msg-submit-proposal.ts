b5d04118d830b199f7aa044cf0edabf08c5439c8
user <hiroaking717@gmail.com>
2019-09-02 17:57:38 +0900

    [update]govのmsg

--- src/x/gov/types/msg-submit-proposal.ts
+++ src/x/gov/types/msg-submit-proposal.ts
@@ -1,7 +1,31 @@
 import { Amino } from "../../../common/amino";
 import { Msg } from "../../../types/cosmos-sdk/msg";
+import { AccAddress } from "../../../types/cosmos-sdk/address/acc-address";
+import { Content } from "./content";
+import { Coin } from "../../../types/cosmos-sdk/coin";
 
 @Amino.RegisterConcrete('cosmos-sdk/MsgSubmitProposal')
 export class MsgSubmitProposal implements Msg {
+    /** 
+    * @param content
+    * @param initial_deposit
+    * @param proposer
+    */
+    constructor(
+        public content: Content,
+        public initial_deposit: Coin,
+        public proposer: AccAddress
+    ) { }
 
+    /**
+    * @see Amino.reviver
+    * @param obj
+    */
+    public static fromJSON(obj: any) {
+        return new this(
+            obj.content,
+            obj.initial_deposit,
+            AccAddress.fromBech32(obj.proposer)
+        );
+    }
 }
