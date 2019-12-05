5c919019fb79c6f54fe52598f2753e9b44fc5e8a
bcts369 <41810380+bcts369@users.noreply.github.com>
2019-12-05 19:55:20 +0800

    add register codec for bank module

--- src/x/bank/index.ts
+++ src/x/bank/index.ts
@@ -3,6 +3,9 @@ import { SendReq } from "./types/send-req";
 import { StdTx } from "../auth/types/std-tx";
 import { Coin } from "../../types/cosmos-sdk/coin";
 import { AccAddress } from "../../types/cosmos-sdk/address/acc-address";
+import { Amino } from "../../common/amino";
+import { MsgSend } from "./types/msg-send";
+import { MsgMultiSend } from "./types/msg-multi-send";
 
 export * from "./types";
 
@@ -10,6 +13,14 @@ export * from "./types";
  * Cosmos SDKにおけるx/bankのRest APIをまとめたモジュール。
  */
 export namespace Bank {
+  /**
+   * Register codec
+   */
+  export function init() {
+    Amino.RegisterConcrete('cosmos-sdk/MsgSend', MsgSend);
+    Amino.RegisterConcrete('cosmos-sdk/MsgMultiSend', MsgMultiSend);
+  }
+
   /**
    * /bank/accounts/${address}/transfers
    * @param sdk
