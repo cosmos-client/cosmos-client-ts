b9670da7905656b9ed07e01bf20760eaa0bcda73
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-03-26 00:48:36 +0900

    chore: lint

--- bundle.js
+++ bundle.js
@@ -1,9 +1,9 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const sources = ["LICENSE", "package-lock.json", "package.json", "README.md"];
+const sources = ['LICENSE', 'package-lock.json', 'package.json', 'README.md'];
 
 for (const source of sources) {
   fs.copyFileSync(source, `dist/${source}`);
 }
-fs.copyFileSync("src/proto.js", `dist/proto.js`);
-fs.copyFileSync("src/proto.d.ts", `dist/proto.d.ts`);
+fs.copyFileSync('src/proto.js', `dist/proto.js`);
+fs.copyFileSync('src/proto.d.ts', `dist/proto.d.ts`);
