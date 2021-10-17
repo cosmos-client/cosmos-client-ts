4b544603d039fb6a44a40996b8bcb43ba901ed20
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-10-17 18:44:50 +0900

    chore: register codec

--- src/rest/authz/index.ts
+++ src/rest/authz/index.ts
@@ -6,3 +6,5 @@ export * as authz from './module';
 codec.register('/cosmos.authz.v1beta1.MsgGrant', cosmos.authz.v1beta1.MsgGrant);
 codec.register('/cosmos.authz.v1beta1.MsgExec', cosmos.authz.v1beta1.MsgExec);
 codec.register('/cosmos.authz.v1beta1.MsgRevoke', cosmos.authz.v1beta1.MsgRevoke);
+
+codec.register('/cosmos.authz.v1beta1.GenericAuthorization', cosmos.authz.v1beta1.GenericAuthorization);
