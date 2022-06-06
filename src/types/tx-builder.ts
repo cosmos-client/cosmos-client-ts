1d816fff4c39111f3e6591c1a98290899f2c5115
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2022-06-06 21:56:11 +0900

    Update tx-builder.ts

--- src/types/tx-builder.ts
+++ src/types/tx-builder.ts
@@ -59,6 +59,28 @@ export class TxBuilder {
   }
 
   cosmosJSONStringify(space?: number) {
-    return JSON.stringify(this.packCosmosAny(), undefined, space);
+    return JSON.stringify(canonicalizeJSON(this.packCosmosAny()), undefined, space);
+  }
+  
+  canonicalizeJSON(value: any): any {
+    if (Object.prototype.toString.call(value) === "[object Object]") {
+      const sorted = {} as { [key: string]: any };
+      const keys = Object.keys(value).sort();
+
+      for (const key of keys) {
+        const keyValue = value[key];
+        if (keyValue != null) {
+          sorted[key] = canonicalizeJSON(keyValue);
+        }
+      }
+
+      return sorted;
+    }
+
+    if (Array.isArray(value)) {
+      return value.map(canonicalizeJSON);
+    }
+
+    return value === undefined ? null : value;
   }
 }
