a9e96c603ae7af8a33a7113587555f46a8dd6faf
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2022-06-21 10:10:03 +0900

    feat: compact bit array test

---
+++ src/types/compact-bit-array.spec.ts
@@ -0,0 +1,12 @@
+import { proto } from '..';
+
+describe('compact-bit-array', () => {
+  it('elems', async () => {
+    expect.hasAssertions();
+
+    const array = proto.cosmos.crypto.multisig.v1beta1.CompactBitArray.from([true, true]);
+
+    expect(array?.extra_bits_stored).toBe(2);
+    expect(Buffer.from(array?.elems!).toString('base64')).toBe('wA==');
+  });
+});
