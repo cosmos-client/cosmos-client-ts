65b976d0a78e4c9647408bf7fc2175f78056fba2
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-09-27 17:58:52 +0900

    feat: govV1

--- src/rest/gov/module.ts
+++ src/rest/gov/module.ts
@@ -1,10 +1,11 @@
-import { QueryApi } from '../../openapi/api';
+import { GovV1ProposalProposalStatusEnum, QueryApi } from '../../openapi/api';
 import { CosmosSDK } from '../../sdk';
 
 export function params(sdk: CosmosSDK, paramsType: string) {
   return new QueryApi(undefined, sdk.url).govParams(paramsType);
 }
 
+// deprecated gov/v1beta1
 export function proposals(
   sdk: CosmosSDK,
   proposalStatus?:
@@ -32,10 +33,39 @@ export function proposals(
   );
 }
 
+export function govV1Proposals(
+  sdk: CosmosSDK,
+  proposalStatus?: GovV1ProposalProposalStatusEnum,
+  voter?: string,
+  depositor?: string,
+  paginationKey?: string,
+  paginationOffset?: string,
+  paginationLimit?: string,
+  paginationCountTotal?: boolean,
+  paginationReverse?: boolean,
+) {
+  return new QueryApi(undefined, sdk.url).govV1Proposal(
+    proposalStatus,
+    voter,
+    depositor,
+    paginationKey,
+    paginationOffset,
+    paginationLimit,
+    paginationCountTotal,
+    paginationReverse,
+  );
+}
+
+// deprecated gov/v1beta1
 export function proposal(sdk: CosmosSDK, proposalID: string) {
   return new QueryApi(undefined, sdk.url).proposal(proposalID);
 }
 
+export function govV1Proposal(sdk: CosmosSDK, proposalID: string) {
+  return new QueryApi(undefined, sdk.url).govV1Proposal_2(proposalID);
+}
+
+// deprecated gov/v1beta1
 export function deposits(
   sdk: CosmosSDK,
   proposalID: string,
@@ -53,14 +83,44 @@ export function deposits(
   );
 }
 
+export function govV1Deposits(
+  sdk: CosmosSDK,
+  proposalID: string,
+  paginationKey?: string,
+  paginationOffset?: string,
+  paginationLimit?: string,
+  paginationCountTotal?: boolean,
+  paginationReverse?: boolean,
+) {
+  return new QueryApi(undefined, sdk.url).govV1Deposit(
+    proposalID,
+    paginationKey,
+    paginationOffset,
+    paginationLimit,
+    paginationCountTotal,
+    paginationReverse
+  );
+}
+
+// deprecated gov/v1beta1
 export function deposit(sdk: CosmosSDK, proposalID: string, depositor: string) {
   return new QueryApi(undefined, sdk.url).deposit(proposalID, depositor);
 }
 
+export function govV1Deposit(sdk: CosmosSDK, proposalID: string, depositor: string) {
+  return new QueryApi(undefined, sdk.url).govV1Deposit_1(proposalID, depositor);
+}
+
+// deprecated gov/v1beta1
 export function tallyresult(sdk: CosmosSDK, proposalID: string) {
   return new QueryApi(undefined, sdk.url).tallyResult(proposalID);
 }
 
+export function govV1TallyResult(sdk: CosmosSDK, proposalID: string) {
+  return new QueryApi(undefined, sdk.url).govV1TallyResult(proposalID);
+}
+
+// deprecated gov/v1beta1
 export function votes(
   sdk: CosmosSDK,
   proposalID: string,
@@ -78,6 +138,31 @@ export function votes(
   );
 }
 
-export function vote(sdk: CosmosSDK, proposalID: string, voter: string) {
-  return new QueryApi(undefined, sdk.url).vote(proposalID, voter.toString());
+export function govV1Votes(
+  sdk: CosmosSDK,
+  proposalID: string,
+  paginationKey?: string,
+  paginationOffset?: string,
+  paginationLimit?: string,
+  paginationCountTotal?: boolean,
+  paginationReverse?: boolean,
+) {
+  return new QueryApi(undefined, sdk.url).govV1Votes(
+    proposalID,
+    paginationKey,
+    paginationOffset,
+    paginationLimit,
+    paginationCountTotal,
+    paginationReverse
+  );
 }
+
+// deprecated gov/v1beta1
+export function vote(sdk: CosmosSDK, proposalID: string, voter: string) {
+  return new QueryApi(undefined, sdk.url).vote(proposalID, voter);
+}
+
+export function govV1Vote(sdk: CosmosSDK, proposalID: string, voter: string) {
+  return new QueryApi(undefined, sdk.url).govV1Vote(proposalID, voter);
+}
+
