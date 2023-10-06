163c87d037608d2e3dd2b8e602f8e1064f93e3d4
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-10-06 17:06:59 +0900

    feat: msg

--- src/rest/gov/index.ts
+++ src/rest/gov/index.ts
@@ -4,7 +4,9 @@ import { codec } from '../../types';
 export * as gov from './module';
 
 codec.register('/cosmos.gov.v1.MsgDeposit', cosmos.gov.v1.MsgDeposit);
-codec.register('/cosmos.gov.v1beta1.MsgSubmitProposal', cosmos.gov.v1beta1.MsgSubmitProposal);
-codec.register('/cosmos.gov.v1.MsgVoteWeighted', cosmos.gov.v1.MsgVoteWeighted);
+codec.register('/cosmos.gov.v1.MsgExecLegacyContent', cosmos.gov.v1.MsgExecLegacyContent);
+codec.register('/cosmos.gov.v1.MsgSubmitProposal', cosmos.gov.v1.MsgSubmitProposal);
+codec.register('/cosmos.gov.v1.MsgUpdateParams', cosmos.gov.v1.MsgUpdateParams);
 codec.register('/cosmos.gov.v1.MsgVote', cosmos.gov.v1.MsgVote);
+codec.register('/cosmos.gov.v1.MsgVoteWeighted', cosmos.gov.v1.MsgVoteWeighted);
 codec.register('/cosmos.gov.v1beta1.TextProposal', cosmos.gov.v1beta1.TextProposal);
